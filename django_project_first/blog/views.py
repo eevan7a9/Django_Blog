acc8726ae7dbe910e214fa7a47036a856b5a0436
eevan7a9 <e7a9@protonmail.com>
2019-03-29 11:07:18 +0800

    added views to render templates

--- django_project_first/blog/views.py
+++ django_project_first/blog/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+dummy_post = [
+    {'title':'Hello World',
+     'body':'This is My first Post, thank you',
+     'created':'2019, March, 29'},
+    {'title':'Have a good day',
+     'body':'This is My second Post, thank you',
+     'created':'2019, March, 30'},
+]
 
 def home_page(request):  # don't forget to add request as argument
     # return HttpResponse('<h1>Home Page </h1>')
-    return render(request, 'blog/home.html')  # .templates/blog/home.html
+    context = {
+        'posts':dummy_post,
+    }
+    return render(request, 'blog/home.html', context)  # .templates/blog/home.html
 
 def about_page(request):
     return render(request, 'blog/about.html')  # .templates/blog/about.html
+
