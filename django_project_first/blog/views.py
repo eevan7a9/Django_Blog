7bc66100c0c44e4ced0ef919a19af0f4fa3f2865
eevan7a9 <e7a9@protonmail.com>
2019-04-07 20:02:27 +0900

    added .django to extended page

--- django_project_first/blog/views.py
+++ django_project_first/blog/views.py
@@ -16,9 +16,8 @@ def home_page(request):  # don't forget to add request as argument
     context = {
         'posts': dummy_post,  # posts will be the key word we use on the html template
     }
-    return render(request, 'blog/home.html', context)  # .templates/blog/home.html
+    return render(request, 'blog/home.html.django', context)  # .templates/blog/home.html
 
 
 def about_page(request):
-    return render(request, 'blog/about.html', {'title': 'About Page'})  # .templates/blog/about.html
-
+    return render(request, 'blog/about.html.django', {'title': 'About Page'})  # .templates/blog/about.html
