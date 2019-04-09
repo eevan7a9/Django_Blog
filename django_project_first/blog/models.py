3e49101cde33afb0654fef1b18cd0434af91a0d4
eevan7a9 <e7a9@protonmail.com>
2019-04-09 22:05:45 +0900

    my first migration

--- django_project_first/blog/models.py
+++ django_project_first/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.utils import timezone  # we need this coz we use timezone for 'created_at'
+from django.contrib.auth.models import User  # we need this to be able to set user as primary key
 # Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	created_at = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
