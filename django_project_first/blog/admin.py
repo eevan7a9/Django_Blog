eea5f7187a5580a7ca2cc7ba8ffbc6e378c11f84
eevan7a9 <e7a9@protonmail.com>
2019-04-15 00:25:01 +0900

    added models.Post to views.py and models.py

--- django_project_first/blog/admin.py
+++ django_project_first/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post  #to be able to access the posts
 
+admin.site.register(Post)
 # Register your models here.
--- django_project_first/blog/views.py
+++ django_project_first/blog/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-dummy_post = [
-    {'title': 'Hello World',
-     'body': 'This is My first Post, thank you',
-     'created': '2019 March-29'},
-    {'title': 'Have a good day',
-     'body': 'This is My second Post, thank you',
-     'created': '2019 March 30'},
-]
+from .models import Post
 
 
 def home_page(request):  # don't forget to add request as argument
     # return HttpResponse('<h1>Home Page </h1>')
     context = {
-        'posts': dummy_post,  # posts will be the key word we use on the html template
+        'posts': Post.objects.all(),  # posts will be the key word we use on the html template
     }
     # return render(request, 'blog/home.html.django', context)  # .templates/blog/home.html
     return render(request, 'blog/home.html', context)
