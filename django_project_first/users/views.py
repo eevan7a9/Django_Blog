ba83e4bac1bcef0458a5386fa28ce3169648b87b
eevan7a9 <e7a9@protonmail.com>
2019-04-19 21:10:24 +0900

    customize user form, added forms.py in the users app

--- django_project_first/users/views.py
+++ django_project_first/users/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm  # to use for creating user registration form
-from  django.shortcuts import redirect  # to be able to redirect to other pages after command
+# from django.contrib.auth.forms import UserCreationForm  # to use for creating default user registration form
+from django.shortcuts import redirect  # to be able to redirect to other pages after command
 from django.contrib import messages  # messages.debug, messages.info, messages.success
-
+from .forms import UserRegistrationForm
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':  # we check to see if POST method are bring used
-        form = UserCreationForm(request.POST)  # if POST then we must verify the request
+        form = UserRegistrationForm(request.POST)  # if POST then we must verify the request
         if form.is_valid():  # if validation is success
+            form.save()  # to save the form into the database
             username = form.cleaned_data.get('username')  # get the validated data
-            messages.success(request, f'Account created for {username}')
+            messages.success(request, 'Accounts created {}'.format(username))
             return redirect('blog-home_page')
     else:
-        form = UserCreationForm()  # if not POST then nothing to verify
+        form = UserRegistrationForm()  # if not POST then nothing to verify
 
     context = {
         'form': form,
